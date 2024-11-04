a00f276501a294e2c18a32104f1322310cdaf2e3
dandirivaldimointi404 <152193667+dandirivaldimointi404@users.noreply.github.com>
2024-11-04 13:48:17 +0800

    11111

--- resources/views/penghuni/edit.blade.php
+++ resources/views/penghuni/edit.blade.php
@@ -53,15 +53,7 @@
                                         </div>
 
                                     </div>
-                                    <div class="form-group">
-                                        <label for="password">Password</label>
-                                        <input type="password" class="form-control @error('password') is-invalid @enderror"
-                                            id="password" name="password" placeholder="Password"
-                                            value="{{ old('password') }}">
-                                        @error('password')
-                                            <div class="invalid-feedback">{{ $message }}</div>
-                                        @enderror
-                                    </div>
+                                   
 
                                     <div class="row">
                                         <div class="form-group col-lg-6" hidden>
@@ -74,6 +66,16 @@
                                         </div>
 
                                         <div class="form-group col-lg-6">
+                                            <label for="password">Password</label>
+                                            <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                                id="password" name="password" placeholder="Password"
+                                                value="{{ old('password') }}">
+                                            @error('password')
+                                                <div class="invalid-feedback">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+
+                                        <div class="form-group col-lg-6" style="display: none">
                                             <label for="alamat">Alamat</label>
                                             <input type="text" class="form-control @error('alamat') is-invalid @enderror"
                                                 id="alamat" name="alamat" placeholder="Masukkan Alamat"
